b86f7894fd43a589d9cc12044e46ec77e50f603d
Om Sitapara <omsitapara23@gmail.com>
2018-09-29 19:26:53 +0530

    Add project files.

---
+++ Sparky-core/Character.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <ArrowsIoEngine\SpriteBatch.h>
+#include <ArrowsIoEngine\ResourceManager.h>
+#include <ArrowsIoEngine\GLTexture.h>
+#include <string>
+#include <vector>
+#include "Bullet.h"
+#include "Level.h"
+#include "Sockets.h"
+
+const int MIN_WALL_DISTANCE = 3;
+using namespace ArrowsIoEngine;
+class Character
+{
+public:
+	Character(std::string name, glm::vec2 pos, int person, glm::vec2 dim, float speed, const std::vector<std::string>& levelData);
+	~Character();
+	void draw(SpriteBatch& spriteBatch);
+	void moveUP(float vertical_speed);
+	void moveDOWN();
+	void moveRIGHT();
+	void moveLEFT();
+	// make private
+	int m_time_rem = 0;
+	float air_pos_x = getPosition().x, air_pos_y = getPosition().y;
+	bool is_d_pressed = false;
+	bool is_a_pressed = false;
+	float h_speed = 0;
+	bool c1 = false;
+	bool c2 = false;
+	int m_is_called_by = 1;
+	bool m_hasReachedGround = true;
+	bool m_first_time = true;
+	int m_direction = -1;	// 0 means steady, -1 means down and 1 means up
+
+	bool damageTaken(int damage, int livePlayer, int callerPlayer, int playerType);
+	void setGunType(int i) { gunID = i; }
+	void setLife(bool v) { life = v; }
+	//getters
+	glm::vec2 getPosition() { return (m_position + glm::vec2(m_dim.x / 2, m_dim.y / 2)); }	//dimension added to get position of the centre of the character
+	int getHealth() { return m_health; }
+	glm::vec2 getDim() { return m_dim; }
+	void increaseHealth();
+	std::string getData();
+	std::string getNameScore() { return m_name + "\t\t\t" + std::to_string(m_score); }
+	//setters
+	void setData(float x, float y, int health);
+	void setHeart(int heart);
+	bool getLife() { return life; }
+	int getGunType() { return gunID; }
+
+private:
+	glm::vec2 m_position, m_dim;
+	glm::vec4 m_uv = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+	Color m_color = { 255,255,255,255 };
+	std::string m_name;
+	int m_person;
+	GLuint m_texId;	//stores the ids of the textures. non-shooting mode, shooting mode
+	int m_state;	//stores if it is in shooting mode or non shooting mode
+	int m_vertical_speed = 0;
+	float m_speed;
+	float m_gravity = -1.8;
+	int m_health = 7;
+	int m_score = 0;
+	std::vector<std::string> m_levelData;
+	
+	int gunID;
+	int m_currentGunIndex;
+	bool life = true;
+
+	std::string m_filePaths[4] = { "../Sparky-core/Textures/Characters/char1.png","../Sparky-core/Textures/Characters/char2.png","../Sparky-core/Textures/Characters/char3.png","../Sparky-core/Textures/Characters/char4.png" };
+};
+
---
+++ Sparky-core/Level.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <ArrowsIoEngine/SpriteBatch.h>
+
+const int TILE_WIDTH = 45;
+
+class Level
+{
+public:
+	Level(const std::string& fileName, int screenWidth, int screenHeight);
+	~Level();
+
+	void draw();
+	//getters
+	std::vector<std::string> getLevelData() { return m_levelData; }
+
+private:
+	std::vector<std::string> m_levelData;
+	ArrowsIoEngine::SpriteBatch m_spriteBatch;
+	GLuint brick1Id;
+	GLuint brick2Id;
+	GLuint grassId;
+	GLuint floorId;
+	GLuint spikeMinusId;
+	GLuint spikePlusId;
+	GLuint spikeUpId;
+	GLuint spikeDownId;
+	GLuint spikeHalfId;
+	GLuint spikeSmalId;
+};
